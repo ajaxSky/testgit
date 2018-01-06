29a22a4889344c1422d3be1ded987bcffa5cf95c
ajaxSky <1414867149@qq.com>
2018-01-06 20:29:35 +0800

    Update MongoTest.java

--- mongoTest/src/cn/hcx/Test/MongoTest.java
+++ mongoTest/src/cn/hcx/Test/MongoTest.java
@@ -6,7 +6,7 @@ import com.mongodb.MongoClient;
 
 public class MongoTest {
 	public static void main(String[] args) {
-		MongoClient client = new MongoClient("192.168.52.128");
+		MongoClient client = new MongoClient("192.168.x.X");
 		List<String> databaseNames = client.getDatabaseNames();
 		for (String dbName : databaseNames) {
 			System.out.println(dbName);
